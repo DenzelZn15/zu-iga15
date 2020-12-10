717468b450a4b2f71ad56a10e2262978be802443
DenzelZn15 <75793943+DenzelZn15@users.noreply.github.com>
2020-12-10 09:13:34 -0500

    Add files via upload

---
+++ css/codigo.js
@@ -0,0 +1,13 @@
+//DESARROLAR EL CODIGO DE LOS EJERCICIOS PROPUESTOS MEDIANTE
+//FUNCIONES
+
+//desarrollo 01
+fucion mostrar(){
+    alert("demostracion de llamar a una funcion");
+}
+
+
+//desarrollo 02
+
+
+//desarrollo 03
